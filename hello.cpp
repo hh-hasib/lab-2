90de2635fe7a62959c6ffdf90004b28675ca4a93
hh-hasib <hhhasib01@gmail.com>
2024-01-24 09:14:49 +0600

    First Commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gap " "
+#define nl "\n"
+#define ll long long
+#define yy cout << "YES\n"
+#define nn cout << "NO\n"
+#define gcd __gcd
+#define tt            \
+    int totalcase;    \
+    cin >> totalcase; \
+    for (int caseno = 1; caseno <= totalcase; caseno++)
+
+int main()
+{
+
+    return 0;
+}
