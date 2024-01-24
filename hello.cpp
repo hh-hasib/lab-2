93b1d2c4ae0c734d8b491c44a9f892e7253fc798
hh-hasib <hhhasib01@gmail.com>
2024-01-24 09:39:08 +0600

    Singleton

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define gap " "
-#define nl "\n"
-#define ll long long
-#define yy cout << "YES\n"
-#define nn cout << "NO\n"
-#define gcd __gcd
-#define tt            \
-    int totalcase;    \
-    cin >> totalcase; \
-    for (int caseno = 1; caseno <= totalcase; caseno++)
+
+class Shape
+{
+public:
+    virtual void draw() = 0;
+};
+
+class Rectangle : public Shape
+{
+public:
+    void draw() override
+    {
+        cout << "Inside Rectangle." << endl;
+    }
+};
+
+class Square : public Shape
+{
+public:
+    void draw() override
+    {
+        cout << "Inside Square." << endl;
+    }
+};
+
+class Circle : public Shape
+{
+public:
+    void draw() override
+    {
+        cout << "Inside Circle." << endl;
+    }
+};
+
+class ShapeFactory
+{
+private:
+    ShapeFactory() {}
+
+public:
+    static ShapeFactory &getInstance()
+    {
+        static ShapeFactory instance;
+        return instance;
+    }
+
+    Shape *getShape(const string &shapeType)
+    {
+        if (shapeType == "RECTANGLE")
+        {
+            return new Rectangle();
+        }
+        else if (shapeType == "SQUARE")
+        {
+            return new Square();
+        }
+        else if (shapeType == "CIRCLE")
+        {
+            return new Circle();
+        }
+        return nullptr;
+    }
+};
 
 int main()
 {
+    ShapeFactory &shapeFactory = ShapeFactory::getInstance();
+
+    Shape *shape1 = shapeFactory.getShape("CIRCLE");
+    if (shape1 != nullptr)
+    {
+        shape1->draw();
+        delete shape1;
+    }
+
+    Shape *shape2 = shapeFactory.getShape("RECTANGLE");
+    if (shape2 != nullptr)
+    {
+        shape2->draw();
+        delete shape2;
+    }
 
     return 0;
 }
